c493c6d8f76eebe4534af8195db0af155d8f6811
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-03-05 22:17:24 -0500

    first commit

---
+++ Firebase Example/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Firebase Example
+//
+//  Created by Adolfo Lozano Mendez on 5/03/18.
+//  Copyright © 2018 Adolfo Lozano Mendez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
